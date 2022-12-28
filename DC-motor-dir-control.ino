63ee193774beb1972b1d02cfe3b6ec72b0fb8d6c
Syahirah Anuar <80741610+syahirahanuar03@users.noreply.github.com>
2022-12-28 11:09:10 +0800

    Declare-name-update

--- DC-motor-dir-control.ino
+++ DC-motor-dir-control.ino
@@ -5,14 +5,12 @@ const int pb3 = A2; //CW
 const int pb4 = A3; //CCW
 
 //MOTOR1
-int in1 = 11;
-int in2 = 12;
-int enA1 = 13;
-
-//MOTOR2
-int in3 = 8;
-int in4 = 9;
-int enA2 = 10;
+int in1 = 8;
+int in2 = 9;
+int in3 = 10;
+int in4 = 11;
+int enA = 12;
+int enB = 13;
 
 void setup() 
 {
@@ -26,30 +24,29 @@ void setup()
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);  
-  pinMode(enA1, OUTPUT);
-  pinMode(enA2, OUTPUT);
-
-  analogWrite(enA1, 100); // Send PWM signal to L298N Enable pin
-  analogWrite(enA2, 100);
+  pinMode(in4, OUTPUT);
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
 }
 
 void loop() 
 {
+  analogWrite(enA, 50); // Send PWM signal to L298N Enable pin
+  
   ////////////////DC MOTOR LAUNCHER///////////////////////
   if (digitalRead(pb3) == HIGH)
   {
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
     digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH); 
+    digitalWrite(in4, HIGH);
   }
-  else if(digitalRead(pb4) == LOW)
+  else if(digitalRead(pb4) == HIGH)
   {
     digitalWrite(in1, HIGH);
     digitalWrite(in2, LOW);
     digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW); 
+    digitalWrite(in4, LOW);
   }
 
   delay(500);
