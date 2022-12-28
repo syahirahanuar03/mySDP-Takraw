7ad33ee5c70109e774361dc7ec00ecd4767bcaf7
Syahirah Anuar <80741610+syahirahanuar03@users.noreply.github.com>
2022-12-28 15:22:53 +0800

    new_update

--- DC-motor-dir-control.ino
+++ DC-motor-dir-control.ino
@@ -1,53 +1,113 @@
 //*********************************************************************************//
 //**********************DC MOTOR - DIRECTION CONTROL****************************************//
 
-const int pb3 = A2; //CW
-const int pb4 = A3; //CCW
 
 //MOTOR1
-int in1 = 8;
-int in2 = 9;
-int in3 = 10;
-int in4 = 11;
-int enA = 12;
-int enB = 13;
+int IN1 = 7; // IN1 L298 ==> pin 7 
+int IN2 = 8; // IN2 ==> Pin8
+int ENA = 5; // Enable A ==> Pin9 PWM
+
+//MOTOR2
+int IN3 = 9; // IN1 L298 ==> pin 7 
+int IN4 = 10; // IN2 ==> Pin8
+int ENB = 6; // Enable A ==> Pin9 PWM
+
+//PUSH BUTTON
+int Pb1 = 11; // Push button 1 ==> Pin 11 
+int Pb2 = 12; // Push button 2 ==> pin 12
+int Pb3 = 13; // Push button 3 ==> pin 13 (STOP) (imaginary) 
 
 void setup() 
 {
-  // initialize the serial port:
-  Serial.begin(9600);
-
-  pinMode(pb3, INPUT);
-  pinMode(pb4, INPUT);
+  //MOTOR 1
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENA, OUTPUT);
   
-  // Set the output pins
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
-  pinMode(enA, OUTPUT);
-  pinMode(enB, OUTPUT);
+  //MOTOR 1
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  
+  // Set the INPUT pins
+  pinMode(Pb1, INPUT_PULLUP);  // Default = HIGH  
+  pinMode(Pb2, INPUT_PULLUP);
+  pinMode(Pb3, INPUT_PULLUP);	//Default = LOW
+  
+  analogWrite(ENA, 0); 
+  analogWrite(ENB, 0);
 }
 
 void loop() 
 {
-  analogWrite(enA, 50); // Send PWM signal to L298N Enable pin
+   while ((digitalRead(Pb1) == HIGH) && (digitalRead(Pb2) == HIGH) && (digitalRead(Pb3) == LOW))
+  {
+    MotorSpeed = 0;
+  }
+  
+  boolean Pb1P = digitalRead(Pb1); // PB1P = Push button 1 Pressed
+  boolean Pb2P = digitalRead(Pb2);
+  boolean Pb3P = digitalRead(Pb3); 
   
   ////////////////DC MOTOR LAUNCHER///////////////////////
-  if (digitalRead(pb3) == HIGH)
-  {
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-  }
-  else if(digitalRead(pb4) == HIGH)
-  {
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
-  }
+ if( Pb1P == LOW)
+ {
+   // Forward 
+   digitalWrite(IN1, HIGH);
+   digitalWrite(IN2, LOW);
+   digitalWrite(IN3, HIGH);
+   digitalWrite(IN4, LOW);
+   
+   if( MotorSpeed < 200)
+   
+   {
+     MotorSpeed +=5;
+     analogWrite(ENA,MotorSpeed);
+     analogWrite(ENB,MotorSpeed);
+     delay(100);
+   }
+   else 
+   
+   {
+     MotorSpeed = 100;
+     analogWrite(ENA, MotorSpeed);
+     analogWrite(ENB, MotorSpeed);
+   }
+ }
+ 
+ 
+ else if( Pb2P == LOW)
+ {
+    // Reverse 
+   digitalWrite(IN1, LOW);
+   digitalWrite(IN2, HIGH);
+   digitalWrite(IN3, LOW);
+   digitalWrite(IN4, HIGH);   
+   
+   if( MotorSpeed < 200)
+   
+   {
+     MotorSpeed +=5;
+     analogWrite(ENA,MotorSpeed);
+     analogWrite(ENB,MotorSpeed);
+     delay(100);
+   }
+   else 
+   {
+     MotorSpeed = 100;
+     analogWrite(ENA, MotorSpeed);
+     analogWrite(ENB, MotorSpeed);
+   }
+ }
+ 
+ else if( Pb3P == HIGH)
+ {
+ 
+   MotorSpeed = 0;
+   analogWrite(ENA, MotorSpeed);
+   analogWrite(ENB, MotorSpeed);
+   
+ }
 
-  delay(500);
+}
 }
